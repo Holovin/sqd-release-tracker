6d7f4b869970179f2cae76f855ea0d0546c923b9
Alex Holovin <alex@holov.in>
2024-07-05 15:08:41 +0200

    v1.03: vercel hooks support; formatting changes

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,9 @@
+import { format } from 'date-fns';
+
 export function isDev(): boolean {
     return process.env.NODE_ENV !== 'production';
+}
+
+export function formatDate(date: Date): string {
+    return format(date, 'HH:mm:ss dd-MM-yyyy')
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import Fastify from 'fastify';
 import fastifyCors from '@fastify/cors';
 import { readFileSync } from 'fs';
-import { isDev } from './helpers.js';
+import { formatDate, isDev } from './helpers.js';
 import { logger } from './logger';
 import { bot } from './telegramBot';
+import * as crypto from 'crypto';
 
 
 export const server = Fastify({
@@ -22,17 +23,53 @@ server.register(fastifyCors, {
 });
 
 server.post('/heroku', async function handler(request, reply) {
-    const resp = request.body as any;
-    const appName = resp.data?.app?.name ?? '(app)'
-    const userName = resp.data?.user?.email ?? '(user)';
-    const status = resp.data?.status ?? '[status]';
-    const time = new Date(resp.data?.created_at ?? '0');
-    const description = resp.data?.description ?? '[info]';
+    const payload = request.body as any;
+    const appName = payload.data?.app?.name ?? '(app)'
+    const userName = payload.data?.user?.email ?? '(user)';
+    const status = payload.data?.status ?? '[status]';
+    const date = formatDate(payload.data?.created_at ?? '0');
+    const description = payload.data?.description ?? '-';
 
-    await bot.send('Heroku', `[${appName}] ${userName} -- ${status} -- ${time}\n${description}`);
+    await bot.send('Heroku', `${date}\n[${appName}] -- ${status} \nby ${userName}\nExtra: ${description}`);
 
-    return reply.status(200).send({ 'status': 'OK '});
+    return reply.status(200).send({ 'status': 'OK' });
 });
+
+server.post('/vercel', async function handler(request, reply) {
+    if (request.headers['x-vercel-signature']) {
+        const signature = crypto
+            .createHmac('sha1', process.env.VERCEL_HOOK_SECRET)
+            .update(JSON.stringify(request.body))
+            .digest('hex');
+
+        if (signature !== request.headers['x-vercel-signature']) {
+            return reply.status(200);
+        }
+    }
+
+    const payload = request.body as any;
+    const date = formatDate(payload.createdAt ?? '0');
+    const type = payload.type;
+    let extra = '';
+
+    if ([
+        'deployment.created',
+        'deployment.succeeded',
+        'deployment.ready',
+        'deployment.promoted',
+        'deployment.canceled',
+        'deployment.error',
+    ].includes(type)) {
+        extra += `Project: ${payload.deployment?.name} (${payload.deployment?.url ?? 'no url'})` +
+                 `\nTarget: ${payload.target ?? 'null'}`;
+    }
+
+
+    await bot.send('Vercel', `${date}\n${type}\n${extra}`);
+
+    return reply.status(200).send({ 'status': 'OK' });
+});
+
 
 server.setNotFoundHandler(async function notFound(request, reply) {
     return reply
